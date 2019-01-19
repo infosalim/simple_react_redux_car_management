20741267992fd6e4375766ab27993df31044b761
infosalim <salimh5757@gmail.com>
2019-01-19 23:56:19 +0600

    created search

--- src/containers/search.js
+++ src/containers/search.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword: ''
+        }
+    }
+
+
+    handleChange = event => {
+        this.setState({
+            keyword: event.target.value
+        })
+    }
+    searchCar = event => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+    componentDidMount() {
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div className='main-search'>
-                <form>
-                    <input type="text"/>
+                <form onSubmit={this.searchCar}>
+                    <input
+                        type="text"
+                        placeholder='Search Here...'
+                        onChange={this.handleChange}
+                        value={this.state.keyword} />
                 </form>
             </div>
         );
