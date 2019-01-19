3329296b71d46a59d2af83c8258b49451c5411b9
infosalim <salimh5757@gmail.com>
2019-01-19 23:40:21 +0600

    created components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+// components
+import Search from '../containers/search';
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-       Simple redux app
+          <Search/>
       </div>
     );
   }
---
+++ src/components/car.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Car extends Component {
+    render() {
+        return (
+            <div>
+                Sam
+            </div>
+        );
+    }
+}
+
+export default Car;
