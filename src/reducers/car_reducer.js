a6d77dcfc3c6b4c83f403af8fdb4dd2e53f27035
infosalim <salimh5757@gmail.com>
2019-01-21 22:39:32 +0600

    added the reducer

--- src/reducers/car_reducer.js
+++ src/reducers/car_reducer.js
@@ -1,6 +1,10 @@
 
 
-export default function(state = null, action){
-    console.log(action);
-    return state;
+export default function(state = [], action){
+    switch(action.type){
+        case 'SEARCH_CARS':
+            return action.payload
+        default:
+            return state
+    }
 }
