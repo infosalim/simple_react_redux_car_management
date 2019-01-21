2bd7fea80361ee2539ed6643ce9593d40f2c5b5e
infosalim <salimh5757@gmail.com>
2019-01-21 22:32:08 +0600

    added redux bind,middleware and promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
 
 
+const URL_ROOT = 'http://localhost:3004';
+
 export function getCars(keywords){
-    console.log(`Made a request with the ${keywords} KET...`);
+
+    const request = fetch(`${URL_ROOT}/carsIndex?q=${keywords}`, {method: 'GET'})
+        .then(response => response.json())
+
+    return {
+        type: 'SEARCH_CARS',
+        payload: request
+    }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import promiseMiddleware from 'redux-promise';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 import './app.css';
@@ -13,7 +14,7 @@ import App from './components/App';
 import Car from './components/car';
 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
